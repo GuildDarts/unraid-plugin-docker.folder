cd48dbd900e065d8d85a82948d67a433f2f267e9
GuildDarts <12391692+GuildDarts@users.noreply.github.com>
2020-04-24 12:46:13 +0200

    Only restart started dockers

--- source/docker.folder/usr/local/emhttp/plugins/docker.folder/scripts/docker_default_cmd.php
+++ source/docker.folder/usr/local/emhttp/plugins/docker.folder/scripts/docker_default_cmd.php
@@ -1,9 +1,30 @@
 <?php
 
-$action = $_GET["action"];
-$containers = json_decode("${_GET["containers"]}",true);
+// stuff for checking if container is stopped (then dont restart)
+$docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
+require_once "$docroot/plugins/dynamix.docker.manager/include/DockerClient.php";
 
-foreach ($containers as $container) {
-    exec("docker $action $container");
+// Read container info
+$DockerClient    = new DockerClient();
+$DockerTemplates = new DockerTemplates();
+$containers      = $DockerClient->getDockerContainers();
+$allInfo = $DockerTemplates->getAllInfo();
+
+
+$action = $_GET["action"];
+$iContainers = json_decode("${_GET["containers"]}",true);
+
+foreach ($iContainers as $iContainer) {
+    if ($action == "restart") {
+        foreach ($containers as $ct) {
+            $name = $ct['Name'];
+            $info = &$allInfo[$name];
+            $running = $info['running'] ? 1 : 0;
+            if ($iContainer == $name && $running == 1) {
+                exec("docker $action $iContainer");
+            }
+        }
+    } else {
+        exec("docker $action $iContainer");
+    }
 }
-?>
