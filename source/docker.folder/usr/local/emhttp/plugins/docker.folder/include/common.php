fe318f5a7b036aa62b89aa8daa86f128ce1dfa1a
GuildDarts <12391692+GuildDarts@users.noreply.github.com>
2020-06-16 14:39:28 +0200

    Fix folders sometimes not loading

--- source/docker.folder/usr/local/emhttp/plugins/docker.folder/include/common.php
+++ source/docker.folder/usr/local/emhttp/plugins/docker.folder/include/common.php
@@ -4,17 +4,20 @@ require_once("$docroot/plugins/docker.folder/include/folderVersion.php");
 require_once("$docroot/plugins/dynamix.docker.manager/include/DockerClient.php");
 
 $user_prefs      = $dockerManPaths['user-prefs'];
-if (file_exists($user_prefs)) {
-    $prefs = json_encode(parse_ini_file($user_prefs));
-} else {
-    $prefs = json_encode([]);
-}
 
 $DockerClient    = new DockerClient();
 $DockerTemplates = new DockerTemplates();
 $containers      = $DockerClient->getDockerContainers();
 $allInfo         = $DockerTemplates->getAllInfo();
 
+if (file_exists($user_prefs)) {
+    $prefs = parse_ini_file($user_prefs); $sort = [];
+    foreach ($containers as $ct) $sort[] = array_search($ct['Name'],$prefs) ?? 999;
+    array_multisort($sort,SORT_NUMERIC,$containers);
+    unset($sort);
+}
+
+$dockers = [];
 $dockerIds = new stdClass;
 $dockerAutostart = new stdClass;
 
@@ -26,6 +29,8 @@ foreach ($containers as $ct) {
 
     $dockerIds->$name = $id;
     $dockerAutostart->$name = $is_autostart;
+
+    array_push($dockers, $name);
 }
 
 echo "<script>var dockerIds = " . json_encode($dockerIds) . ';</script>';
@@ -307,23 +312,23 @@ echo "<script>foldersVersion = " . $GLOBALS['foldersVersion'] . ';</script>';
             var folderTemplate = `<tr class="sortable docker-folder-parent-${folderName}"><td class="ct-name" style="width:220px;padding:8px;"><div><span class="outer"><span class="hand" id="folder-${folderName}"><img src="/plugins/dynamix.docker.manager/images/question.png?1587731339" class="img"></span><span class="inner"><span class="appname ">${folderName}</span><br><i class="fa fa-square stopped red-text"></i><span class="state">folder</span></span></span></td><td class="updatecolumn"></td><td colspan="3" class="dockerPreview"></td><td class="advanced" style="display: table-cell;"><span class="cpu">USAGE</span><div class="usage-disk mm"><span id="cpu" style="width: 0%;"></span><span></span></div><br><span class="mem">USAGE</span></div></td><td></td><td></td></tr>`
         }
 
-        var perfs = <?= $prefs ?>;
+        var dockers = <?= json_encode($dockers) ?>;
         var insertIndex = 0
-        // insert at start if not in perfs
-        if (!perfs.includes(`${folderName}-folder`)) {
+        // insert at start if not in dockers
+        if (!dockers.includes(`${folderName}-folder`)) {
             insertAtIndex(insertIndex, folderTemplate, selector, selectorType)
         } else {
-            for (i = 0; i < perfs.length; i++) {
-                if (perfs[i] == `${folderName}-folder`) {
+            for (i = 0; i < dockers.length; i++) {
+                if (dockers[i] == `${folderName}-folder`) {
                     insertAtIndex(insertIndex, folderTemplate, selector, selectorType)
                     break
                 }
-                if (folderChildren.includes(perfs[i])) {
+                if (folderChildren.includes(dockers[i])) {
                     continue
                 }
                 // continue incase folder does not get remove from userprefs (better safe than sorry)
                 let folderNames = Object.keys(folders)
-                if (perfs[i].includes('-folder') && !folderNames.includes(perfs[i].slice(0, -7))) {
+                if (dockers[i].includes('-folder') && !folderNames.includes(dockers[i].slice(0, -7))) {
                     continue
                 }
                 insertIndex++
