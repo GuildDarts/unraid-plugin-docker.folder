c30254c23d1d0eafc53dd6b4a0e6e88621ac2f5c
GuildDarts <12391692+GuildDarts@users.noreply.github.com>
2020-07-01 23:03:33 +0200

    Fix renaming folder reseting sort order & cleanup

--- source/docker.folder/usr/local/emhttp/plugins/docker.folder/scripts/save_folder.php
+++ source/docker.folder/usr/local/emhttp/plugins/docker.folder/scripts/save_folder.php
@@ -1,56 +1,30 @@
 <?php
-/*
-   $folder_file = json_decode(file_get_contents("/boot/config/plugins/docker.folder/results.json"),true);
-
-   $response = json_decode("${_GET["settings"]}",true);
-   $name = $response["name"];
-
-   unset($response{'name'});
-
-   $output = new stdClass();
-   $fp->$name = $response;
-
-   $fp = fopen('/boot/config/plugins/docker.folder/results.json', 'w');
-   fwrite($fp, json_encode($output, JSON_PRETTY_PRINT));
-   fclose($fp);
-*/
-?>
-
-
-<?php
     $response = json_decode("${_POST["settings"]}",true);
     
-    $name = $response["name"];
-    unset($response{'name'});
+    $name = $response['name'];
+    unset($response['name']);
 
     $inp = file_get_contents('/boot/config/plugins/docker.folder/folders.json');
     $tempObj = json_decode($inp);
 
     $tempObj->$name = $response;
 
-    // remove old key if renaming folder
-    $rename = $_POST["rename"];
+    // remove old key if renaming folder & rename folder in userprefs
+    $rename = $_POST['rename'];
     if (isset($rename)) {
         unset($tempObj->$rename);
+
+        $path = '/boot/config/plugins/dockerMan/userprefs.cfg';
+        if (file_exists($path)) {
+            $userprefs = parse_ini_file($path,true);
+            $index = array_search($rename.'-folder', $userprefs);
+            $userprefs[$index] = $name.'-folder';
+            write_php_ini($userprefs, $path);
+        }
     }
 
     $jsonData = json_encode($tempObj, JSON_PRETTY_PRINT);
     file_put_contents('/boot/config/plugins/docker.folder/folders.json', $jsonData);
-?>
-
-
-<?php
-
-    $path = "/boot/config/plugins/dockerMan/userprefs.cfg";
-
-    $userprefs = parse_ini_file($path,true);
-    $fName = $name."-folder";
-    if (in_array($fName, $userprefs) == null) {
-        array_unshift($userprefs, $fName);
-        write_php_ini($userprefs, $path);
-    }
-
-
 
     function write_php_ini($array, $file) {
         $res = array();
